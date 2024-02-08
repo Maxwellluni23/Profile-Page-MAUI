285523c1d270c498f40ef334142a51be8bee6549
Maxwellluni23 <159486261+Maxwellluni23@users.noreply.github.com>
2024-02-08 22:20:55 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+{
+    // Increment the counter
+    count++;
+
+    // Update the text of the button based on the counter value
+    if (count == 1)
+        CounterBtn.Text = "Something awesome will happen";
+    else
+        CounterBtn.Text = "A bit of patience... Incoming";
+
+    // Use SemanticScreenReader to announce the updated text for accessibility
+    SemanticScreenReader.Announce(CounterBtn.Text);}
+}
+
